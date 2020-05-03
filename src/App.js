3b5a43fb9c8ac3b699a1fedca8c2af61ffed88ac
jsphthms <josephreverethomas@gmail.com>
2020-05-02 17:28:39 -0700

    Inital commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
+import Cards from './components/Cards/Cards'
+import Chart from './components/Chart/Chart'
+import CountryPicker from './components/CountryPicker/CountryPicker'
+import { fetchData } from './api'
+
+import styles from './App.module.css'
 
 function App() {
+
+  const [data, setData] = useState([]);
+  const [country, setCountry] = useState('');
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setData(await fetchData(country));
+    }
+    fetchAPI(country);
+    console.log(data)
+  }, [country]);
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setData(await fetchData());
+    }
+    fetchAPI();
+  }, []);
+
+
+  const handleCountryChange = async (country) => {
+    setCountry(country)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <h1>Covid-19 Tracker</h1>
+      <Cards data={data} />
+      <CountryPicker handleCountryChange={handleCountryChange} />
+      <Chart data={data} country={country} />
+
     </div>
   );
 }
